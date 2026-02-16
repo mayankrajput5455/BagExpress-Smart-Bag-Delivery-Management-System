8035dcc809f955540bbe48abd44f338c9840e988
“Mayank <mayankrajput5455@gmail.com>
2026-02-16 14:54:18 +0530

    files changed

---
+++ backend/src/app.js
@@ -0,0 +1,38 @@
+import express from "express";
+import cors from "cors";
+
+const app = express()
+
+
+// basic configuration
+
+app.use(express.json({ limit : "16kb" }));
+app.use(express.urlencoded({ extended: true, limit : "16kb" }));
+app.use(express.static("public"));
+
+
+// cors configuration
+
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+        credentials:true,
+        methods: ["GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    }),
+);
+
+// import the routes
+
+import healthCheckRouter from "./routes/healthcheck.routes.js";
+import authRouter from "./routes/auth.routes.js"
+
+app.use("/api/v1/healthcheck", healthCheckRouter)
+app.use("/api/v1/auth", authRouter)
+
+app.get("/",(req, res) =>{
+    res.send("This is homepage");
+})
+
+
+export default app
---
+++ backend/src/controllers/healthcheck.controller.js
@@ -0,0 +1,11 @@
+import {ApiResponse} from "../utils/api-response.js"
+import { asyncHandler } from "../utils/async-handler.js";
+
+
+const healthCheck = asyncHandler(async (req,res)=>{
+    res.status(200).json(
+        new ApiResponse(200, {message: "Server is Running"})
+    );
+})
+
+export { healthCheck };
