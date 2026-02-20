4de5e4397996d62c92615675f7dd84910dcf6bb4
TarkeshwarGupta <tarkeshwar11101@gmail.com>
2026-02-20 18:37:41 +0530

    frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>le beta</h1>
+      <h3>aake duniya me bhi agar pyaar na kiya toa kya kiya.</h3>
+    </>
+  )
+}
+
+export default App
